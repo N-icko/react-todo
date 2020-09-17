f08f39f73d029b6bba89d2d53cbb7d2e32f94257
N-icko <nickolay.kuzmin.dev@gmail.com>
2020-09-17 11:28:36 +0300

    Updates

---
+++ precache-manifest.8a49472b2bdf6927e521eeee390842b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bd242f8fc124eb976f74fe28e648e66",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "43f822d02fcc26120d05",
+    "url": "/react-todo/static/css/main.a2e613d3.chunk.css"
+  },
+  {
+    "revision": "9d86434fef7e6b874e03",
+    "url": "/react-todo/static/js/2.e5a0cd9d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo/static/js/2.e5a0cd9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43f822d02fcc26120d05",
+    "url": "/react-todo/static/js/main.7c539228.chunk.js"
+  },
+  {
+    "revision": "8b60092a6ae08cc228b0",
+    "url": "/react-todo/static/js/runtime-main.40cbbb06.js"
+  }
+]);
